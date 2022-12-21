95619ce6a3700b736827b63a39aaacc285d37a75
ianMuchesia <ianmuchesia@yahoo.com>
2022-12-21 12:35:33 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+
+
+function App() {
+
+  const getAge = (name: string): number=>{
+    return 99;
+  }
+  type Person={
+    name: string ;
+    age? : number;
+  }
+
+
+ 
+  
+  return (
+    <div className="App">
+     <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App

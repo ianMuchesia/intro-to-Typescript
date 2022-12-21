2cea1a73335b32d39fa65ef276eac560ca21a793
ianMuchesia <ianmuchesia@yahoo.com>
2022-12-21 16:25:49 +0300

    final commit

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./styles.css";
+import { Todo } from "../Model";
+import SingleTodo from "./SingleTodo";
+
+interface Props {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
+  return (
+    <div>
+      {todos.map((todo) => (
+        <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos} />
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
